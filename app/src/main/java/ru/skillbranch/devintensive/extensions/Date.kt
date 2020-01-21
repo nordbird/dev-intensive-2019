6e87ce329da880d047bd3af056e7fa423d07095e
NevedrovAS <nevedrov@mineframe.ru>
2020-01-21 11:27:33 +0300

    Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -25,6 +26,30 @@ fun Date.add(value: Int, units: TimeUnits): Date {
 
     this.time = time
     return this
+}
+
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val delta = date.time - this.time
+    val absDelta = delta.absoluteValue
+
+    val base = when (absDelta) {
+        in 0..SECOND -> "только что"
+        in SECOND..45 * SECOND -> "несколько секунд"
+        in 45 * SECOND..75 * SECOND -> "минуту"
+        in 75 * SECOND..45 * MINUTE -> "${absDelta / MINUTE} минут"
+        in 45 * MINUTE..75 * MINUTE -> "час"
+        in 75 * MINUTE..22 * HOUR -> "${absDelta / HOUR} часов"
+        in 22 * HOUR..26 * HOUR -> "день"
+        in 26 * HOUR..360 * DAY -> "${absDelta / DAY} дней"
+        else -> ""
+    }
+
+    return when {
+        delta <- 360 * DAY -> "более чем через год"
+        delta > 360 * DAY -> "более года назад"
+        delta < 0 -> "через $base"
+        else -> "$base назад"
+    }
 }
 
 enum class TimeUnits {
