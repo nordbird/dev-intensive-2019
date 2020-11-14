694aaceeca90a7dbd0b06b55867e9011c8720863
NevedrovAS <nevedrov@mineframe.ru>
2020-11-14 12:46:31 +0300

    Activity.hideKeyboard

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,15 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    val view = this.currentFocus ?: View(this)
+    
+    imm?.hideSoftInputFromWindow(view.windowToken, 0)
+    view.clearFocus();
+}
+
