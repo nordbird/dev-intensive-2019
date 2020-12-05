ebfca25ba48d105898cab06aa7b2faedf949ba46
NevedrovAS <nevedrov@mineframe.ru>
2020-12-05 21:33:39 +0300

    MainActivity: Search by Title in RecyclerView

--- app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -3,6 +3,9 @@ package ru.skillbranch.devintensive.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -30,8 +33,41 @@ class MainActivity : AppCompatActivity() {
         initViewModel()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        val searchItem = menu?.findItem(R.id.action_search)
+        val searchView = searchItem?.actionView as SearchView
+
+        searchView.queryHint = "Введите имя пользователя"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                viewModel.handleSearchQuery(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                viewModel.handleSearchQuery(newText)
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return if (item?.itemId == android.R.id.home) {
+            finish()
+            overridePendingTransition(R.anim.idle, R.anim.bottom_down)
+            true
+        } else {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun initToolbar() {
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     private fun initViews() {
--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
@@ -1,35 +1,42 @@
 package ru.skillbranch.devintensive.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import ru.skillbranch.devintensive.extensions.mutableLiveData
 import ru.skillbranch.devintensive.models.data.ChatItem
 import ru.skillbranch.devintensive.repositories.ChatRepository
 
 class MainViewModel : ViewModel() {
+    private val query = mutableLiveData("")
     private val chatRepository = ChatRepository
-    private val chats = Transformations.map(chatRepository.loadChats()) { chats ->
+    private val chatItems = Transformations.map(chatRepository.loadChats()) { chats ->
         return@map chats.filter { !it.isArchived }
                 .map { it.toChatItem() }
                 .sortedBy { it.id.toInt() }
     }
 
     fun getChatData(): LiveData<List<ChatItem>> {
-        return chats
+        val result = MediatorLiveData<List<ChatItem>>()
+
+        val filterF = {
+            val queryStr = query.value!!
+            val chats = chatItems.value!!
+
+            result.value = if (queryStr.isEmpty()) chats
+            else chats.filter { it.title.contains(queryStr, true) }
+        }
+
+        result.addSource(chatItems) { filterF.invoke() }
+        result.addSource(query) { filterF.invoke() }
+
+        return result
     }
 
-//    private fun loadChats(): List<ChatItem> {
-//        val chats = chatRepository.loadChats()
-//        return chats.map { it.toChatItem() }.sortedBy { it.id.toInt() }
-//    }
-//
-//    fun addItems() {
-//        val newItems = DataGenerator.generateChatsWithOffset(chats.value!!.size, 5).map { it.toChatItem() }
-//        val copy = chats.value!!.toMutableList()
-//        copy.addAll(newItems)
-//        chats.value = copy.sortedBy { it.id.toInt() }
-//
-//    }
+    fun handleSearchQuery(text: String?) {
+        query.value = text.orEmpty()
+    }
 
     fun addToArchive(chatId: String) {
         val chat = chatRepository.find(chatId)
